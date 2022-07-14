e49f5dc5550a7bc85839b854891b78d1087b6b4f
Dhruv Jain <101544395+Dhruv0443@users.noreply.github.com>
2022-07-14 16:46:35 +0530

    Initializing the input var

--- deleting ele in array at any pos(8jul).cpp
+++ deleting ele in array at any pos(8jul).cpp
@@ -2,23 +2,23 @@
 using namespace std;
 int main()
 {
-    int n,i,e,p;
+    int number,i,pos;
     cout<<"Enter number of elements want in array = ";
-    cin>>n;
-    int arr[n];
+    cin>>number;
+    int arr[number];
     cout<<"Enter array's elements: ";
-    for (i=0;i<n;i++)
+    for (i=0;i<number;i++)
     {
         cin>>arr[i];
     }
     cout<<"Enter the position where num to be delete: = ";
-    cin>>p;
-    for(i=p-1;i<n;i++)
+    cin>>pos;
+    for(i=p-1;i<number;i++)
     {
           arr[i]=arr[i+1];
     }
     cout<<"The new array is = ";
-    for (i=0;i<n-1;i++)
+    for (i=0;i<number-1;i++)
     {
         cout<<arr[i]<<" ";
 
